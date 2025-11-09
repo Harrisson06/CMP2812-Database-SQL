3d6ff3e1094a6d4e2558f66efb4904d27488c2dd
Harrisson06 <28958802@students.lincoln.ac.uk>
2025-11-09 03:20:36 +0000

    made data / fixed some errors / made more

--- CMP2812_28958802_Databases.sql
+++ CMP2812_28958802_Databases.sql
@@ -1,9 +1,18 @@
 # NEW YORK STATE PATROL DEPT
+# Creating the Database and then using the database
 CREATE DATABASE NYSPD;
 USE NYSPD;
 
-GRANT ALL ON NYSPD.* TO "Officer_User";
-GRANT Select On NYSPD.ViolationInformation TO "Public_User";
+### Commands that dont work the way i want 
+# Creates both the officer profile and the Public profile 
+# CREATE USER "Officer"@"host" IDENTIFIED BY "password";
+#Create user "Public"@"localhost" identified by "Public";
+
+# Allows all access to the Officer profile 
+# Only allows ViolationInformation to Public profile 
+#GRANT SELECT ON NYSPD.* TO "Officer"@"localhost";
+#GRANT VIEW ON NYSPD.ViolationInformation TO "Public_User"@"localhost";
+###
 
 # Creating the table that hold Driver Information
 CREATE TABLE DriverInformation (
@@ -14,7 +23,7 @@ City tinytext,
 State tinytext,
 ZipCode smallint,
 DriverLicense int,
-StateIssued tinytext,
+StateIssuedLicense tinytext,
 BirthDate date,
 Height Decimal(2,2),
 Weight tinyint,
@@ -50,19 +59,18 @@ ViolationDesc tinytext,
 Detachment tinytext,
 
 CONSTRAINT DateCheck CHECK (ViolationDate >= date(2020-01-01)
-AND ViolationDate <= date(2026-01-01)),
+AND ViolationDate <= date(2026-03-03)),
 
 PRIMARY KEY (ViolationID)
-
 );
 
 # Creating the table that holds officer information and actions
 CREATE TABLE OfficerInformation (
-OfficerName varchar(20),
+OfficerLastName varchar(20),
 PersonelNumber int,
 Detachment tinytext,
 
-PRIMARY KEY (OfficerName)
+PRIMARY KEY (PersonelNumber)
 );
 
 # Creating the table that holds Violations 
@@ -70,11 +78,59 @@ PRIMARY KEY (OfficerName)
 CREATE TABLE Violations (
 ViolationID int,
 ViolationDate date,
-OfficerName varchar(20),
+PersonelID int,
 VehicleVIN int,
-DriverLicense int,
 
 FOREIGN KEY (ViolationID) REFERENCES ViolationInformation(ViolationID),
-FOREIGN KEY (OfficerName) REFERENCES OfficerInformation(OfficerName),
+FOREIGN KEY (PersonelID) REFERENCES OfficerInformation(PersonelNumber),
 FOREIGN KEY (VehicleVIN) REFERENCES VehicleInformation(VIN)
 );
+
+# Inserting data into the tables 
+
+#FirstName tinytext,
+#LastName tinytext,
+#Address tinytext,
+#City tinytext,
+#State tinytext,
+#ZipCode smallint,
+#DriverLicense int,
+#StateIssuedLicense tinytext,
+#BirthDate date,
+#Height Decimal(2,2),
+#Weight tinyint,
+#EyeColour tinytext,
+
+
+## DriverInformation Data Creation
+INSERT INTO DriverInformation
+VALUES ("Winter", "Elsey", "8346 Elm Street, Brooklyn", "Wa", "NY"
+
+
+
+
+## OfficerInformation Data Creation
+### Creating Patrol Officers 
+INSERT INTO OfficerInformation
+VALUES ("Angelo", 74361169, "PatrolOfficer");
+INSERT INTO OfficerInformation
+VALUES ("Wright", 8861767, "PatrolOfficer");
+INSERT INTO OfficerInformation
+VALUES ("Truss", 1118262, "PatrolOfficer");
+INSERT INTO OfficerInformation
+VALUES ("Madden", 1721804, "PatrolOfficer");
+
+### Creating Traffic Enforcers 
+INSERT INTO OfficerInformation
+VALUES ("Doss", 7001990, "TrafficEnforcer");
+INSERT INTO OfficerInformation
+VALUES ("Vance", 9004461, "TrafficEnforcer");
+INSERT INTO OfficerInformation
+VALUES ("Pence", 5658260, "TrafficEnforcer");
+
+### Creating HighwayPatrol Officers
+INSERT INTO OfficerInformation
+VALUES ("Danes", 6261011, "HighwayPatrolOfficer");
+INSERT INTO OfficerInformation
+VALUES ("Gates", 8179260, "HighwayPatrolOfficer")
+
